a893a78076f40b122d7edb8a5698f11aa49c0040
Tibo Vanheule <tibo@tibovanheule.space>
2019-09-25 09:54:42 +0200

    working pull script

--- secretAdmin/pull.php
+++ secretAdmin/pull.php
@@ -1,13 +1,17 @@
 <?php
+//error_reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+// function to execute the command and print the result and exit code
 function execPrint($command) {
-    $result = array();
-    exec($command, $result);
-    foreach ($result as $line) print($line . "\n");
+        $result = array();
+        $resultCode = "";
+        exec($command, $result,$resultCode);
+        echo implode("\n", $result);
+        echo $resultCode;
 }
+
 // Print the exec output inside of a pre element
-print("<pre>" . execPrint("git pull") . "</pre>");
-print("<pre>" . execPrint("whoami") . "</pre>");
+print("<pre>" . execPrint("cd ../ && git pull -v --commit") . "</pre>");
 ?>
