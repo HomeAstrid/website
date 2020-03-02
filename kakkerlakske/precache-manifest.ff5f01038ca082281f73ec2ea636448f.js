3a6944a105016c5b422ab77f2e6ba79734980494
Tibo Vanheule <tibo@tibovanheule.space>
2020-03-02 02:12:26 +0100

    new kakkerlakske
    start of nieuw kakkerlakske pagina

---
+++ kakkerlakske/precache-manifest.ff5f01038ca082281f73ec2ea636448f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee47ccd1ffc6f4d0b877f3ea14ba65eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "114fcc00984822883e65",
+    "url": "./static/js/2.24bec942.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.24bec942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800f18d408feddf3a26a",
+    "url": "./static/js/main.4302aae6.chunk.js"
+  },
+  {
+    "revision": "006126e9d9a46b355449",
+    "url": "./static/js/runtime-main.10d5d596.js"
+  }
+]);
