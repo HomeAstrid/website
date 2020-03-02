d3cf7f36109461b879074a5528b980a174e85124
Tibo Vanheule <tibo@tibovanheule.space>
2020-03-02 02:46:45 +0100

    scan dir

---
+++ kakkerlakske/scan_dir.php
@@ -0,0 +1,13 @@
+<?php
+// scan de dir (vind alle bestanden)
+$files = scandir('./uploads/');
+$array = [];
+// overloop en zoek de pdf's
+foreach ($files as $file) {
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+    if ($ext == 'pdf') {
+        $elem = new StdClass();
+        $elem->url = "http://astrid.ugent.be/kakkerlakske/uploads/" . $file;
+        array_push($array, $elem);
+    }
+}
